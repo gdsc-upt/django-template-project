64c0b5080e26c4204323fae9468dadca1dc201d6
Dan Percic <danpercic86@gmail.com>
2020-11-20 20:10:00 +0200

    add staticfiles dirs

--- src/main/settings/base.py
+++ src/main/settings/base.py
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- src/main/settings/production.py
+++ src/main/settings/production.py
@@ -1,3 +1,12 @@
 from .base import *
 
 DEBUG = False
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+SILENCED_SYSTEM_CHECKS = [
+    'security.W004',  # SECURE_HSTS_SECONDS
+    'security.W008',  # SECURE_SSL_REDIRECT
+    'security.W012',  # SESSION_COOKIE_SECURE
+    'security.W016',  # CSRF_COOKIE_SECURE
+]
